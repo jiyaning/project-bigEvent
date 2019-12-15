3ab5b0d7355763001e9d5fc6e724ff8cabc61ca2
木蓝茶陌11 <1824532697@qq.com>
2019-12-15 21:15:22 +0800

    获取用户信息、文章、评论数量

--- web_back/tools/config.js
+++ web_back/tools/config.js
@@ -3,3 +3,7 @@ var BASE_URL = 'http://localhost:8000';
 var LOGIN = BASE_URL + '/admin/login';
 var LOGOUT = BASE_URL + '/admin/logout';
 var GETUSER = BASE_URL + '/admin/getuser';
+var ARTICLE_COUNT = BASE_URL + '/admin/article_count';
+var COMMENT_COUNT = BASE_URL + '/admin/comment_count';
+var GET_USERINFO = BASE_URL + '/admin/userinfo_get';
+var USERINFO_EDIT = BASE_URL + '/admin/userinfo_edit';
